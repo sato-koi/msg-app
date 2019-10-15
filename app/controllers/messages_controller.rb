36235c3fd66ec4f415af0049ecc7e87e0302e239
sato-koi <sato.koichi.0404@gmail.com>
2019-10-15 16:47:47 +0900

    メッセージ改良

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,10 @@ class MessagesController < ApplicationController
   def show
     @msg = params[:msg]
   end
+
+  def show2
+    @msg1 = params[:msg1]
+    @msg2 = params[:msg2]
+    @msg = "#{@msg1}と#{@msg2}を結合"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'messages/:msg' => 'messages#show'
+  get 'messages/:msg1/:msg2' => 'messages#show2'
 end
